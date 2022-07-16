cd94c22db8f4fab167bbb96f407fcc6b06472b76
SHANTANU DAS <thoughtworks.t.das@extaccount.com>
2022-07-16 16:15:03 +0530

    tf yaml for action

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_pet" "rg-name" {
+  prefix    = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name      = random_pet.rg-name.id
+  location  = var.resource_group_location
+}
+
+variable "resource_group_name_prefix" {
+  default       = "rg"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "resource_group_location" {
+  default = "eastus"
+  description   = "Location of the resource group."
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
